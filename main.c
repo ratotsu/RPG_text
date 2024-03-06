f351c7ecdf5b0c9b10a57aebce0a08290588cec5
ratotsu <escapamento1@gmail.com>
2024-03-05 21:38:32 -0300

    save
    Signed-off-by: ratotsu <escapamento1@gmail.com>

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//aqui sera posto tudo q autera o personagem e sua ficha como dano escudo vida e etc...
+
+int main(){
+printf("para começar nossa historia precisamos montar um personagem para vc, vamos la:\n");
+printf("Temos estas racas: \n1-Humano \n2-Elfo \n3- \n0-Escolher \nPara abrir a descricao de cada classe digite o numero correspondente");
+
+return 0;
+}
--- personagem.h
+++ personagem.h
@@ -1,7 +1,50 @@
+//racas escolhidas: humano,elfo,
+//classes escolhidas: barbaro,guerreiro,mago,clerigo,patrulheiro
+
+
+
 #include <stdio.h>
 //aqui sera posto tudo q autera o personagem e sua ficha como dano escudo vida e etc...
 
 void main(){
-printf("inicio da historia");
-// ooo
+    int opcao_raca, opcao_classe, descricao_raca, descricao_classe;
+printf("para começar nossa historia precisamos montar um personagem para vc, vamos la:/n");
+// menu de informacao... racas
+do
+{
+    printf("Temos estas racas: \n1-Humano \n2-Elfo \n3- \n0-Escolher \nPara abrir a descricao de cada classe digite o numero correspondente");
+    scanf("%d", &descricao_raca);
+    switch (descricao_raca)
+    {
+    case 1:
+        /* code */
+        break;
+    case 2:
+        /* code */
+        break;
+    default:
+        break;
+    }
+} while ();
+
+
+// menu basico de escolha de raças e clases
+do{
+switch (opcao_raca)
+{
+    printf("Qual raça vc escolhe?");
+case /* constant-expression */1:
+    /* code */
+    break;
+
+case /* constant-expression */2:
+    /* code */
+    break;
+
+default:
+    break;
+}
+
+}while() 
+
 }
