a28dbd9ef5b9a6ad6577123a4db335b743d7ebb5
ratotsu <escapamento1@gmail.com>
2024-03-06 10:06:20 -0300

    meio-orc
    Signed-off-by: ratotsu <escapamento1@gmail.com>

--- main.c
+++ main.c
@@ -8,7 +8,12 @@ void main()
 {
     int opcao_raca, opcao_classe, descricao_raca, descricao_classe;
     printf("para começar nossa historia precisamos montar um personagem para vc, vamos la:\n\n");
-    // menu de informacao... racas
+
+
+
+
+
+    // menu de informacao... racas, separar em funcao.
     do
     {
         printf("Temos estas racas: \n1-Humano \n2-Elfo \n3- \n0-Escolher \nPara abrir a descricao de cada classe digite o numero correspondente\n\nOpcao:");
@@ -26,28 +31,38 @@ void main()
             //(Vantagem) vai funcionar da seguinte maneira gera dois numeros aleatorios de 1 a 20 e pega o maior deles e atribui os modificadoes do jogador
             //(Desvantagem) funciona da mesma maneira porem o inverso na quatao dos dados se pega o menor e atribui os modificadores
             break;
+        case 3:
+            printf("Atributos Raciais: Os meio-orcs possuem força física impressionante e uma resistência inata. Eles recebem um bônus de +2 em Força e um bônus de +1 em Constituição.\n");
+            printf("Além disso, os meio-orcs são resistentes a situações adversas. Quando estão abaixo de 0 pontos de vida, mas não mortos, eles ficar de pé e continuar lutando, permanecendo conscientes e agindo normalmente.\n(só morre se a vida estiver negativa igual a metade da vida total)\n\n");
+            // para um personagem cair ele tem que chegar a 0, ja para morrer a vida tem que ser negativa igual a metade do total, (cair n se aplica ao meio-orc).
+            break;
         default:
             break;
         }
-    } while (descricao_raca!=0);
+    } while (descricao_raca != 0);
 
-    // menu basico de escolha de raças e clases
+    // menu basico de escolha de raças e classes separar  em funcao
     do
     {
         switch (opcao_raca)
         {
             printf("Qual raça vc escolhe?");
         case 1:
-            /* code */
+            printf("vc é um humano");
+            //aqui é implementada os modificadores da raça escolhida  
             break;
 
-        case  2:
-            /* code */
+        case 2:
+            printf("vc é um elfo");
+            //aqui é implementada os modificadores da raça escolhida
             break;
-
+        case 3:
+            printf("vc é um meio-orc");
+            //aqui é implementada os modificadores da raça escolhidas
+            break;
         default:
             break;
         }
 
-    } while (descricao_classe!=0);
+    } while (descricao_classe != 0);
 }
